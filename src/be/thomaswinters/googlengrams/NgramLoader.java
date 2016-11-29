8052790c2a2fa4f27d9c2ea00d49c02d77f422ab
T. Winters <info@thomaswinters.be>
2016-11-29 02:38:48 +0100

    Made NgramLoader abstract

--- src/be/thomaswinters/googlengrams/NgramLoader.java
+++ src/be/thomaswinters/googlengrams/NgramLoader.java
@@ -5,12 +5,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public class NgramLoader {
-
-	private final static String acceptedWords = "^[a-zA-Z\\-]+$";
-	private final static Pattern wordPattern = Pattern.compile(acceptedWords);
-	private final static int YEAR = 2008;
-	private final static int MINIMAL_OCCURRENCES = 40;
+public abstract class NgramLoader {
 
 	private final NgramCsvReader reader;
 	private final NgramMySQLConnector connector;
@@ -43,32 +38,5 @@ public class NgramLoader {
 		}
 	}
 
-	public boolean isAlphabetical(String word) {
-		return wordPattern.matcher(word).matches();
-	}
-
-	public boolean isAcceptedYear(int year) {
-		return year == YEAR;
-	}
-
-	public boolean hasEnoughCounts(int count) {
-		return count >= MINIMAL_OCCURRENCES;
-	}
-
-	public boolean shouldStore(List<String> words, int year, int count) {
-		return words.stream().allMatch(e -> isAlphabetical(e) && e.length() > 0) && isAcceptedYear(year)
-				&& hasEnoughCounts(count);
-	}
-
-	public static void main(String[] args)
-			throws NumberFormatException, ClassNotFoundException, URISyntaxException, SQLException {
-		for (int i = 0; i < 100; i++) {
-			NgramLoader loader = new NgramLoader(new NgramCsvReader(System.getenv("ngram_csv_file_prefix") + i + ".csv"),
-					new NgramMySQLConnector(2, System.getenv("ngram_db_host"),
-							Integer.parseInt(System.getenv("ngram_db_port")), System.getenv("ngram_db_username"),
-							System.getenv("ngram_db_password"), System.getenv("ngram_db_databaseName")));
-			loader.execute();
-			System.out.println("Done " + i);
-		}
-	}
+	public abstract boolean shouldStore(List<String> words, int year, int count);
 }
